f20af77f6218bd29e1c8ea0b5f69b4a1155d7e25
Niroshan Sathasivam <nsath19@Hotmail.com>
2026-08-17 02:29:35 +0100

    Delete asset directory

--- asset/js/js/carousel/js/carouselInit.js
+++
@@ -1,7 +0,0 @@
-  $ ( document ) .ready( function (  ) {
-
-    $ ( '#reviews' ) .css ( "maxWidth", $ ( '.column-carousel_reviews-left' ) .innerWidth (  ) + "px" ) ;
-
-    $ ( '#reviews .carousel' ) .reviewsCarousel (  ) ;
-
-  } ) ;
